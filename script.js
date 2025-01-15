e108d456b7a30c4ebc2f87a916cfb1176f280ee9
DimitriDoru <152091503+DimitriDoru@users.noreply.github.com>
2025-01-15 13:14:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Me giving you back rubbs and playing with your hair";
-  gif.src ="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzFsbG9pYjQ0YzVzYjByemZiMjZ1bzBncnppOGw5cmZsaDVjNHJ2aSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/xUA7aVwuBDwtKhspos/giphy.gif";
+  gif.src ="https://tenor.com/bv6r3.gif";
   noBtn.style.display = "none";
   const remainingBtn = document.querySelector('.btn-group button:not(.no-btn)');
     // Center the remaining button by setting margin to auto
