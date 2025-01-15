7e17761f850bcc0874eff820c61b06a86e02b316
DimitriDoru <152091503+DimitriDoru@users.noreply.github.com>
2025-01-15 14:56:44 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,1 +1,28 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
 
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Me giving you back rubbs and playing with your hair";
+  gif.src ="https://giphy.com/embed/naIhzcoCSBZm3FARM4";
+  
+  yesBtn.style.display = "none";
+  const yremainingBtn = document.querySelector('.btn-group button:not(.no-btn)');
+    // Center the remaining button by setting margin to auto
+    yremainingBtn.style.margin = 'auto';
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+  noBtn.style.transition = "all 2s"
+});
